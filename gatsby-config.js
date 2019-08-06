b8c576347b00bff2110698a3408815010b10655b
Eric Montzka <emontzka@sbcglobal.net>
2019-08-06 11:39:36 -0500

    add https to baseUrl

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
          * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "http://emontzka.xyz",
+        baseUrl: "https://emontzka.xyz",
         // The protocol. This can be http or https.
         protocol: "http",
         // Indicates whether the site is hosted on wordpress.com.
